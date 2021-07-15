fb0e4615b13f320938c0b5a6f680d129899601b2
Rafiur Rahman <rrfoundary@gmail.com>
2021-07-15 09:23:07 +0600

    Abstracting container and items

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./app.css";
+import AbstractingContainerAndItems from "./components/abstractingContainerAndItems";
 import FillingSpace from "./components/fillingSpace";
 
 const App = () => {
   return (
     <div>
-      <FillingSpace />
+      <AbstractingContainerAndItems />
     </div>
   );
 };
---
+++ src/components/abstractingContainerAndItems.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+});
+
+const Container = (props) => <Grid container {...props} />;
+const Item = (props) => <Grid item {...props} />;
+
+const AbstractingContainerAndItems = (props) => {
+  const { classes } = props;
+  return (
+    <Container spacing={3}>
+      <Item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>Paper item</Paper>
+      </Item>
+      <Item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>Paper item</Paper>
+      </Item>
+      <Item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>Paper item</Paper>
+      </Item>
+      <Item xs={12} sm={6} md={3}>
+        <Paper className={classes.paper}>Paper item</Paper>
+      </Item>
+    </Container>
+  );
+};
+
+export default withStyles(styles)(AbstractingContainerAndItems);
