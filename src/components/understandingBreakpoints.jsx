dbaca18fb635c9863f07bec187c540a5658861cb
Rafiur Rahman <rrfoundary@gmail.com>
2021-07-15 08:55:48 +0600

    style the breakpoints

--- src/components/understandingBreakpoints.jsx
+++ src/components/understandingBreakpoints.jsx
@@ -4,24 +4,29 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 
 const styles = (theme) => ({
-  paper: {},
+  paper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary,
+    textAlign: "center",
+  },
 });
 
 const UnderstandingBreakpoints = (props) => {
+  const { classes } = props;
   return (
     <div>
-      <Grid container>
+      <Grid container spacing={4}>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper>Grid item</Paper>
+          <Paper className={classes.paper}>Grid item</Paper>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper>Grid item</Paper>
+          <Paper className={classes.paper}>Grid item</Paper>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper>Grid item</Paper>
+          <Paper className={classes.paper}>Grid item</Paper>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <Paper>Grid item</Paper>
+          <Paper className={classes.paper}>Grid item</Paper>
         </Grid>
       </Grid>
     </div>
