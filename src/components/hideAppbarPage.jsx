6b8e76c9b89ff96c31b45dd69d81bd73cc57b74a
Rafiur Rahman <rrfoundary@gmail.com>
2021-07-18 08:22:54 +0600

    Added simple Appbar

---
+++ src/components/hideAppbarPage.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Fade from "@material-ui/core/Fade";
+
+const HideAppbarPage = (props) => {
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>
+          <IconButton color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title">App</Typography>
+          <Button variant="text" color="inherit">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default HideAppbarPage;
