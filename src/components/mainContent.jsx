a92fd134be6a702f9b6dd87f712a633534388b0a
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-13 11:36:15 +0600

    added cart header

--- src/components/mainContent.jsx
+++ src/components/mainContent.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import PersonIcon from "@material-ui/icons/Person";
 
 const styles = (theme) => ({
   card: {
     maxWidth: 400,
-    margin: theme.spacing(4),
-  },
-  cartContent: {
-    padding: 0,
-  },
-  content: {
-    marginTop: theme.spacing(2),
   },
 });
 
@@ -21,12 +17,20 @@ const MainContent = (props) => {
   const { classes } = props;
   return (
     <Card className={classes.card}>
-      <CardContent className={classes.cartContent}>
-        <Typography variant="h4">Main title</Typography>
-        <Typography variant="subtitle">A little more about subject</Typography>
-        <Typography className={classes.content}>
-          Even more information on the subject, contained within the card. You
-          can fit a lot of information here, but don't try to overdo it.
+      <CardHeader
+        title="Rafiur Rahman"
+        subheader="Frontend Developer"
+        avatar={
+          <Avatar>
+            <PersonIcon />
+          </Avatar>
+        }
+      />
+      <CardContent>
+        <Typography variant="caption">Joined 2009</Typography>
+        <Typography>
+          Some filler text about the user. There doesn't have to be a lot - just
+          enough so that the text spans at least two lines.
         </Typography>
       </CardContent>
     </Card>
