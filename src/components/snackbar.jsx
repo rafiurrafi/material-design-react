e0b612e44bff723922ba75c04dc5e4c691df9960
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-13 22:56:39 +0600

    added interactivity to snackbar

--- src/components/snackbar.jsx
+++ src/components/snackbar.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Snackbar from "@material-ui/core/Snackbar";
+import { Button } from "@material-ui/core";
 
 const CustomSnackbar = () => {
-  return <Snackbar open={true} message="Hello burra" />;
+  const [openSnack, setOpenSnack] = useState(false);
+  const onOpenSnack = () => {
+    setOpenSnack(true);
+  };
+  const onCloseSnack = () => {
+    setOpenSnack(false);
+  };
+  return (
+    <>
+      <Button onClick={onOpenSnack} primary>
+        Open snackbar
+      </Button>
+      <Snackbar
+        open={openSnack}
+        message="Hello burra"
+        autoHideDuration={1000}
+        onClose={onCloseSnack}
+        ContentProps={{ variant: "h1" }}
+      />
+    </>
+  );
 };
 export default CustomSnackbar;
