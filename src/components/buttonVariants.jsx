10dc87d1b90bbebc93546ea15cc3f9f5f8c3d44d
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-15 13:26:14 +0600

    added fab component

--- src/components/buttonVariants.jsx
+++ src/components/buttonVariants.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+
 const styles = (theme) => ({
   container: {
     margin: theme.spacing(1),
+  },
+  content: {
+    margin: theme.spacing(1),
+  },
+  fab: {
+    position: "fixed",
+    bottom: 20,
+    right: 30,
   },
 });
 const ButtonVariants = (props) => {
@@ -16,20 +29,36 @@ const ButtonVariants = (props) => {
       direction="column"
       spacing={2}
     >
-      <Grid item>
-        <Button variant="text" color="secondary">
-          Text
+      <Grid container justify="space-around">
+        <Button component={Link} to="/">
+          Home
         </Button>
+        <Button component={Link} to="/about">
+          About
+        </Button>
+        <Button component={Link} to="/contact">
+          Contact
+        </Button>
+        <Fab className={classes.fab}>
+          <AddIcon color="primary" />
+        </Fab>
       </Grid>
-      <Grid item>
-        <Button variant="outlined" color="secondary">
-          Text
-        </Button>
-      </Grid>
-      <Grid item>
-        <Button variant="contained" color="secondary">
-          Text
-        </Button>
+      {/* Grid content */}
+      <Grid container className={classes.content}>
+        <Switch>
+          <Route
+            path="/about"
+            component={() => <Typography variant="h1">About</Typography>}
+          />
+          <Route
+            path="/contact"
+            component={() => <Typography variant="h1">Contact</Typography>}
+          />
+          <Route
+            path="/"
+            component={() => <Typography variant="h1">Home</Typography>}
+          />
+        </Switch>
       </Grid>
     </Grid>
   );
