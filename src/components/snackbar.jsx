87f865f6205517108744c47f170d9ccbd7e297ba
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-15 12:27:26 +0600

    added snackbar with actions

--- src/components/snackbar.jsx
+++ src/components/snackbar.jsx
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
+import { Route, Link } from "react-router-dom";
 import Snackbar from "@material-ui/core/Snackbar";
-import { Button } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
 
-const CustomSnackbar = () => {
-  const [openSnack, setOpenSnack] = useState(false);
-  const onOpenSnack = () => {
-    setOpenSnack(true);
-  };
-  const onCloseSnack = () => {
-    setOpenSnack(false);
-  };
+const CustomSnackbar = (props) => {
+  const [open, setOpen] = useState(false);
   return (
-    <>
-      <Button onClick={onOpenSnack} primary>
-        Open snackbar
-      </Button>
+    <div>
+      <Button onClick={() => setOpen(true)}>Open snackbar</Button>
       <Snackbar
-        open={openSnack}
-        message="Hello burra"
-        autoHideDuration={1000}
-        onClose={onCloseSnack}
-        ContentProps={{ variant: "h1" }}
+        open={open}
+        onClose={() => setOpen(false)}
+        message="All done successfully"
+        action={[
+          <IconButton color="inherit" onClick={() => setOpen(false)}>
+            <CloseIcon />
+          </IconButton>,
+        ]}
       />
-    </>
+    </div>
   );
 };
+
 export default CustomSnackbar;
