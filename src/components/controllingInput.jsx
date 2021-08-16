cd021c2fd984989a8fd5206fff47d1357dd13908
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-16 10:03:25 +0600

    refactor text area field

--- src/components/controllingInput.jsx
+++ src/components/controllingInput.jsx
@@ -3,41 +3,30 @@ import { makeStyles } from "@material-ui/styles";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles((theme) => ({
-  container: { margin: theme.spacing.unit * 3 },
-}));
-
 const ControllingInput = (props) => {
-  const [first, setFirst] = useState("");
-  const [second, setSecond] = useState("");
-  const [third, setThird] = useState("");
-  const classes = useStyles();
+  const [inputs, setInputs] = useState([
+    { id: "first", label: "First", value: "" },
+    { id: "second", label: "Second", value: "" },
+    { id: "third", label: "Third", value: "" },
+  ]);
+  const onChange = ({ target }) => {
+    const newInputs = [...inputs];
+    const index = inputs.findIndex((input) => input.id === target.id);
+    newInputs[index] = { ...inputs[index], value: target.value };
+    setInputs(newInputs);
+  };
   return (
-    <Grid container spacing={4} className={classes.container}>
-      <Grid item>
-        <TextField
-          id="first"
-          label="First"
-          value={first}
-          onChange={(e) => setFirst(e.target.value)}
-        />
-      </Grid>
-      <Grid item>
-        <TextField
-          id="second"
-          label="Second"
-          value={second}
-          onChange={(e) => setSecond}
-        />
-      </Grid>
-      <Grid item>
-        <TextField
-          id="third"
-          label="Third"
-          value={third}
-          onChange={(e) => setThird(e.target.value)}
-        />
-      </Grid>
+    <Grid container spacing={4}>
+      {inputs.map((input) => (
+        <Grid item>
+          <TextField
+            id={input.id}
+            label={input.label}
+            value={input.value}
+            onChange={onChange}
+          />
+        </Grid>
+      ))}
     </Grid>
   );
 };
